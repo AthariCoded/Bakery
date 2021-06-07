af3c367ac85c6c11e59c8eec9ef34e95d85edaf6
AthariCoded <athari.rashed@gmail.com>
2021-06-07 19:17:24 +0300

    task react

---
+++ SummerApps/bakery/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 class="text">
+        Bakery Heaven
+      </h1>
+      <h3 class="text">
+        The place for carbs craving 
+      </h3>
+      <img id="centerPic" src="https://i.ndtvimg.com/i/2017-10/bakeries_620x350_41509023137.jpg" />
+    </div>
+  );
+}
+
+export default App;
