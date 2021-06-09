d48f503f41054371bf722aa478c666fbfdc37914
AthariCoded <athari.rashed@gmail.com>
2021-06-09 17:40:31 +0300

    task style

--- bakeryshop/src/App.js
+++ bakeryshop/src/App.js
@@ -1,28 +1,38 @@
+import Home from "./Home";
+import ProductList from "./components/ProductList";
+import { GlobalStyle } from "./styles";
+import { ThemeProvider } from "styled-components";
 
-import './App.css';
-import products from "./products";
+const theme = {
+  mainColor: "grey",
+  backgroundColor: "yellow",
+};
 
 function App() {
-  const productList = products.map((product) => (
-    <div className= "productbaked" key={product.id}>
-      <img className="product-image" scr={product.image} />
-      <p className="text">{product.name}</p>
-      <p className="text">{product.price} KD</p>
-    </div>
-  ));
-
   return (
     <div>
-    <div>
-      <h1 className="text"> Bakery Heaven </h1>
-      <h4 className="text"> The place for carbs craving </h4>
-
-      <img id="centerPic" src="https://i.ndtvimg.com/i/2017-10/bakeries_620x350_41509023137.jpg" />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Home />
+        <ProductList />
+      </ThemeProvider>
     </div>
-
-    <div className="List">
-      {productList}</div>
-      </div>
   );
 }
 export default App;
+
+/*
+import './App.css';
+
+import Home from "./Home";
+
+function App() { 
+  return (
+    <div>
+       <Home/>
+    </div>
+  );
+}
+export default App;
+
+*/
--- bakeryshop/src/products.js
+++ bakeryshop/src/products.js
@@ -3,13 +3,19 @@ const products = [
         id:1,
         name: "Vanilla Muffin",
         price: 5,
-        image:"https://images.eatsmarter.com/sites/default/files/styles/1600x1200/public/chocolate-chip-vanilla-muffins-467755.jpg"
+        image:"https://bakerbynature.com/wp-content/uploads/2011/05/Blueberry-Muffins-1-of-1.jpg"
     },
     {
         id:2,
         name: "Pecan Pie",
         price: 12,
-        image:"https://www.simplyrecipes.com/thmb/KwoRPvcuIIQBphLKbU995_gED9o=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2014__11__Pecan-Pie-LEAD-1-e189ae6b84724b49b445c55c41a37054.jpg"
+        image:"https://www.theseasonedmom.com/wp-content/uploads/2020/08/Easy-Pecan-Pie-Recipe-1.jpg"
+    },
+    {
+        id:3,
+        name: "Macroons",
+        price: 10,
+        image:"https://image.shutterstock.com/image-photo/colourful-closeup-macroons-on-table-260nw-1369131761.jpg"
     }
 ];
 
